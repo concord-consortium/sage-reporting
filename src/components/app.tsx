b3cd153fdf79062a81ccfeeba37d15eda9735d20
Noah Paessel <knowuh@gmail.com>
2021-04-13 17:51:06 -0400

    I can download CSV files

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,37 +1,49 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { XSLtoCSV } from "../process-xlsx";
-import { CsvToHtmlTable } from "react-csv-to-table";
-export interface IProps {
+// import FileDownload from "js-file-download";
+// import { CsvToHtmlTable } from "react-csv-to-table";
+export interface IAppProps {
   userName: string;
   place: string;
 }
 
-const handleFileChange = async (event:React.ChangeEvent<HTMLInputElement>) => {
-  if(event.target.files) {
-    const numFiles = event.target.files.length
-    const selectedFile = event.target.files.item(0);
-    console.log(`Selected file that is ${selectedFile.size} long`);
-    const arrayBuff = await selectedFile.arrayBuffer()
-    const data = new Uint8Array(arrayBuff);
-    console.log(selectedFile);
-    const csvString = XSLtoCSV(data);
-  }
+export interface IDownloadLinkProps {
+  csvString: string;
+  fileName: string;
 }
 
-export const App = (props: IProps) => {
+const DownloadLink = (props: IDownloadLinkProps) => {
+  const {csvString, fileName} = props;
+  const blob = new Blob([csvString], {type:'text/csv'})
+  const downloadURL = window.URL.createObjectURL(blob);
+  if (csvString && csvString.length > 0) {
+    return (
+      <div>
+        <a href={downloadURL} download={fileName}>
+          download {fileName}
+        </a>
+      </div>
+    )
+  }
+  return(<></>);
+}
+
+export const App = (props: IAppProps) => {
   const [csv, setCsv] = React.useState('');
+  const [fileName, setFileName] = React.useState('');
 
   const handleFileChange = async (event:React.ChangeEvent<HTMLInputElement>) => {
     if(event.target.files) {
-      const numFiles = event.target.files.length
       const selectedFile = event.target.files.item(0);
-      console.log(`Selected file that is ${selectedFile.size} long`);
+      const fileName = selectedFile.name;
+
       const arrayBuff = await selectedFile.arrayBuffer()
       const data = new Uint8Array(arrayBuff);
-      console.log(selectedFile);
       const csvString = XSLtoCSV(data);
+      setFileName(fileName.replace(/\.xlsx?/i, ".csv"));
       setCsv(csvString);
+      // FileDownload(csvString, "out.csv");
     }
   }
 
@@ -41,7 +53,8 @@ export const App = (props: IProps) => {
         Hi {props.userName} from React! Welcome to {props.place}!
       </h1>
       <input type="file" id="input" onChange={handleFileChange}/>
-      <CsvToHtmlTable data={csv} csvDelimiter="," />
+      <DownloadLink csvString={csv} fileName={fileName} />
+      {/* <CsvToHtmlTable data={csv} csvDelimiter="," /> */}
     </>
   );
 };
