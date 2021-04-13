13f87aeb64375b63f082164fa01816f45c9c6fb5
Noah Paessel <knowuh@gmail.com>
2021-04-13 17:18:16 -0400

    can read xls files

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,17 +1,53 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-
-export interface HelloWorldProps {
+import { XSLtoCSV } from "../process-xlsx";
+import { CsvToHtmlTable } from "react-csv-to-table";
+export interface IProps {
   userName: string;
-  lang: string;
+  place: string;
 }
-export const App = (props: HelloWorldProps) => (
-  <h1>
-    Hi {props.userName} from React! Welcome to {props.lang}!
-  </h1>
-);
+
+const handleFileChange = async (event:React.ChangeEvent<HTMLInputElement>) => {
+  if(event.target.files) {
+    const numFiles = event.target.files.length
+    const selectedFile = event.target.files.item(0);
+    console.log(`Selected file that is ${selectedFile.size} long`);
+    const arrayBuff = await selectedFile.arrayBuffer()
+    const data = new Uint8Array(arrayBuff);
+    console.log(selectedFile);
+    const csvString = XSLtoCSV(data);
+  }
+}
+
+export const App = (props: IProps) => {
+  const [csv, setCsv] = React.useState('');
+
+  const handleFileChange = async (event:React.ChangeEvent<HTMLInputElement>) => {
+    if(event.target.files) {
+      const numFiles = event.target.files.length
+      const selectedFile = event.target.files.item(0);
+      console.log(`Selected file that is ${selectedFile.size} long`);
+      const arrayBuff = await selectedFile.arrayBuffer()
+      const data = new Uint8Array(arrayBuff);
+      console.log(selectedFile);
+      const csvString = XSLtoCSV(data);
+      setCsv(csvString);
+    }
+  }
+
+  return(
+    <>
+      <h1>
+        Hi {props.userName} from React! Welcome to {props.place}!
+      </h1>
+      <input type="file" id="input" onChange={handleFileChange}/>
+      <CsvToHtmlTable data={csv} csvDelimiter="," />
+    </>
+  );
+};
+
 
 ReactDOM.render(
-  <App userName="Developer Dingus" lang="TypeScript" />,
+  <App userName="Researcher" place="report converter" />,
   document.getElementById("output")
 );
--- src/process-xlsx.ts
+++ src/process-xlsx.ts
@@ -4,6 +4,21 @@ import * as XLSX from 'xlsx';
 // import * as fs from 'fs';
 // Related Stack Overflow: https://stackoverflow.com/questions/34342425/convert-xls-to-csv-on-the-server-in-node
 
+
+export const XSLtoCSV = (data:Uint8Array) => {
+  const workBook = XLSX.read(data,{type: 'array'});
+  const worksSheetName = workBook.SheetNames[0];
+  console.log(`   ...  converting first worksheet named "${worksSheetName}"`);
+  const workSheet = workBook.Sheets[worksSheetName];
+
+  /* skip the first row */
+  var range = XLSX.utils.decode_range(workSheet['!ref']);
+  range.s.r = 1; // <-- zero-indexed, so setting to 1 will skip row 0
+  workSheet['!ref'] = XLSX.utils.encode_range(range);
+  const csvString = XLSX.utils.sheet_to_csv(workSheet);
+  return csvString;
+}
+
 export const XLSProcessor = (fileName: string) => {
   console.log(`XLSProcessor processing: ${fileName}`);
   const workBook = XLSX.readFile(fileName);
