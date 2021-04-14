fce5a0dbd17277d24c1f79f37662b9bbe94f4a63
Noah Paessel <knowuh@gmail.com>
2021-04-14 15:26:59 -0400

    clean up console output

--- src/index.ts
+++ src/index.ts
@@ -15,14 +15,15 @@ console.clear();
 const XlsFileNames = glob.sync(`${workingDirectory}/*.xlsx`);
 const numFiles = XlsFileNames.length;
 console.log(`Importing report ${numFiles} files from ${workingDirectory}`);
-XlsFileNames.forEach(async (fName) => {
-  console.log(`1: Reading XLSX file: ${fName}`);
+XlsFileNames.forEach(async (fName, index) => {
+  const statusString =`(${index + 1}/${numFiles}) → 1`;
+  console.log(`${statusString} 1: Reading XLSX file: ${fName}`);
   const byteArray = fs.readFileSync(fName);
   const csv = XSLtoCSV(byteArray);
   const csvWithTopo = await ProcessCSVData(csv);
-  console.log(`2: Parsing complete: [${SuccessCount}✔|${FailCount}✖]`)
+  console.log(`${statusString} 2: Parsing complete: [${SuccessCount}✔|${FailCount}✖]`)
   const lastPartOfFile = fName.split('\\').pop().split('/').pop();
   const outPath = `${outputDirectory}/${lastPartOfFile.replace(/\.xlsx?/i, '.csv')}`;
-  console.log(`3: writing CSV output file: → ${outPath}`);
+  console.log(`${statusString} 3: writing CSV output file: → ${outPath}`);
   fs.writeFileSync(outPath, csvWithTopo);
 });
